d7999e2ceb4abae35ade89cd32808accd51f07ac
Shashi Thipparthy <shashi_thipparthy@gallup.com>
2019-02-01 00:30:22 -0600

    reducer test is good

--- src/reducers/__tests__/comments.test.js
+++ src/reducers/__tests__/comments.test.js
@@ -13,5 +13,5 @@ it('handles actions of type SAVE_COMMENT ',()=>{
 
 it('handles action with unknown type '()=>{
     const newState=commentsReducer([], {type: 'JUNK_ACTION'});
-    expect(newst)
+    expect(newState).toEqual([]);
 });
